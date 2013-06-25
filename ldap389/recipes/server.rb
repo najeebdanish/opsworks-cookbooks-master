4da4fe0530a5ac3b10e5aa9a76c3c55a2b460ac4
Palisado <budiha@hotmail.com>
2013-06-25 05:46:31 -0400

    test2_get_s3_file
    adding s3 download

--- ldap389/recipes/server.rb
+++ ldap389/recipes/server.rb
@@ -2,6 +2,73 @@ require 'resolv'
 
 package '389-ds'
 
+# Source accepts the protocol s3:// with the host as the bucket
+# access_key_id and secret_access_key are just that
+s3_file "/etc/dirsrv_etc.tar.gz" do
+  source "s3://opsworks-test01/dirsrv_etc.tar.gz"
+  access_key_id #{node[:ldap389][:access_key_id]}
+  secret_access_key #{node[:ldap389][:secret_access_key]}
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+execute "tar xvfz /etc/dirsrv_etc.tar.gz" do
+  cwd "/etc"
+end
+
+s3_file "/usr/lib64/dirsrv_usrlib64.tar.gz" do
+  source "s3://opsworks-test01/dirsrv_usrlib64.tar.gz"
+  access_key_id #{node[:ldap389][:access_key_id]}
+  secret_access_key #{node[:ldap389][:secret_access_key]}
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+execute "tar xvfz /usr/lib64/dirsrv_usrlib64.tar.gz" do
+  cwd "/usr/lib64"
+end
+
+s3_file "/var/lib/dirsrv/slapd-dcaldap01b_varlibdirsrv.tar.gz" do
+  source "s3://opsworks-test01/slapd-dcaldap01b_varlibdirsrv.tar.gz"
+  access_key_id #{node[:ldap389][:access_key_id]}
+  secret_access_key #{node[:ldap389][:secret_access_key]}
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+execute "tar xvfz /var/lib/dirsrv/slapd-dcaldap01b_varlibdirsrv.tar.gz" do
+  cwd "/var/lib/dirsrv"
+end
+
+s3_file "/var/lock/dirsrv/slapd-dcaldap01b_varlockdirsrv.tar.gz" do
+  source "s3://opsworks-test01/slapd-dcaldap01b_varlockdirsrv.tar.gz"
+  access_key_id #{node[:ldap389][:access_key_id]}
+  secret_access_key #{node[:ldap389][:secret_access_key]}
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+execute "tar xvfz /var/lock/dirsrv/slapd-dcaldap01b_varlockdirsrv.tar.gz" do
+  cwd "/var/lock/dirsrv"
+end
+
+s3_file "/var/log/dirsrv/slapd-dcaldap01b_varlogdirsrv.tar.gz" do
+  source "s3://opsworks-test01/slapd-dcaldap01b_varlogdirsrv.tar.gz"
+  access_key_id #{node[:ldap389][:access_key_id]}
+  secret_access_key #{node[:ldap389][:secret_access_key]}
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+execute "tar xvfz /var/log/dirsrv/slapd-dcaldap01b_varlogdirsrv.tar.gz" do
+  cwd "/var/log/dirsrv"
+end
+
 include_recipe 'ldap389::service'
 
 service 'dirsrv' do

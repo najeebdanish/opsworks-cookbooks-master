012f24baf72faf424ba21d2bfc22c7b189d353fd
Palisado <budiha@hotmail.com>
2013-06-26 04:56:59 -0400

    Adding-s3cmd

--- ldap389/recipes/server.rb
+++ ldap389/recipes/server.rb
@@ -1,73 +1,54 @@
-require 'resolv'
-include_recipe 's3'
-
 package '389-ds'
 
-s3_file "/etc/dirsrv_etc.tar.gz" do
-  source "s3://opsworks-test01/dirsrv_etc.tar.gz"
-  access_key_id #{node[:ldap389][:access_key_id]}
-  secret_access_key #{node[:ldap389][:secret_access_key]}
-  owner "root"
-  group "root"
-  mode 0644
+remote_file "/root/s3cmd-1.0.0-4.1.x86_64.rpm" do
+  source "s3cmd-1.0.0-4.1.x86_64.rpm"
 end
 
-execute "tar xvfz /etc/dirsrv_etc.tar.gz" do
-  cwd "/etc"
+execute 'install s3cmd rpm' do
+  cwd '/root/'
+  command "rpm -i s3cmd-1.0.0-4.1.x86_64.rpm"
+  action :run
+  only_if "rpm -qa|grep s3cmd"
 end
 
-s3_file "/usr/lib64/dirsrv_usrlib64.tar.gz" do
-  source "s3://opsworks-test01/dirsrv_usrlib64.tar.gz"
-  access_key_id #{node[:ldap389][:access_key_id]}
-  secret_access_key #{node[:ldap389][:secret_access_key]}
-  owner "root"
-  group "root"
-  mode 0644
+template '/root/.s3cfg' do
+  source 's3cfg.erb'
+  backup false
+  owner 'root'
+  group 'root'
+  mode 0600
 end
 
-execute "tar xvfz /usr/lib64/dirsrv_usrlib64.tar.gz" do
-  cwd "/usr/lib64"
+execute 'install s3cmd rpm' do
+  cwd '/root/'
+  command "rpm -i s3cmd-1.0.0-4.1.x86_64.rpm"
+  action :run
+  only_if "rpm -qa|grep s3cmd"
 end
 
-s3_file "/var/lib/dirsrv/slapd-dcaldap01b_varlibdirsrv.tar.gz" do
-  source "s3://opsworks-test01/slapd-dcaldap01b_varlibdirsrv.tar.gz"
-  access_key_id #{node[:ldap389][:access_key_id]}
-  secret_access_key #{node[:ldap389][:secret_access_key]}
-  owner "root"
-  group "root"
-  mode 0644
+script "Get all 389 files" do
+  interpreter "bash"
+  user "root"
+  cwd "/etc/"
+  code <<-EOH
+  s3cmd get s3://opsworks-test01/dirsrv_etc.tar.gz
+  tar -zxf dirsrv_etc.tar.gz
+  cd /usr/lib64
+  s3cmd get s3://opsworks-test01/dirsrv_usrlib64.tar.gz
+  tar -zxf dirsrv_usrlib64.tar.gz
+  cd /var/lib/dirsrv
+  s3cmd get s3://opsworks-test01/slapd-dcaldap01b_varlibdirsrv.tar.gz
+  tar -zxf slapd-dcaldap01b_varlibdirsrv.tar.gz
+  cd /var/lock/dirsrv
+  s3cmd get s3://opsworks-test01/slapd-dcaldap01b_varlockdirsrv.tar.gz
+  tar -zxf slapd-dcaldap01b_varlockdirsrv.tar.gz
+  cd /var/log/dirsrv
+  s3cmd get s3://opsworks-test01/slapd-dcaldap01b_varlogdirsrv.tar.gz
+  tar -zxf slapd-dcaldap01b_varlogdirsrv.tar.gz
+  EOH
 end
 
-execute "tar xvfz /var/lib/dirsrv/slapd-dcaldap01b_varlibdirsrv.tar.gz" do
-  cwd "/var/lib/dirsrv"
-end
-
-s3_file "/var/lock/dirsrv/slapd-dcaldap01b_varlockdirsrv.tar.gz" do
-  source "s3://opsworks-test01/slapd-dcaldap01b_varlockdirsrv.tar.gz"
-  access_key_id #{node[:ldap389][:access_key_id]}
-  secret_access_key #{node[:ldap389][:secret_access_key]}
-  owner "root"
-  group "root"
-  mode 0644
-end
-
-execute "tar xvfz /var/lock/dirsrv/slapd-dcaldap01b_varlockdirsrv.tar.gz" do
-  cwd "/var/lock/dirsrv"
-end
-
-s3_file "/var/log/dirsrv/slapd-dcaldap01b_varlogdirsrv.tar.gz" do
-  source "s3://opsworks-test01/slapd-dcaldap01b_varlogdirsrv.tar.gz"
-  access_key_id #{node[:ldap389][:access_key_id]}
-  secret_access_key #{node[:ldap389][:secret_access_key]}
-  owner "root"
-  group "root"
-  mode 0644
-end
-
-execute "tar xvfz /var/log/dirsrv/slapd-dcaldap01b_varlogdirsrv.tar.gz" do
-  cwd "/var/log/dirsrv"
-end
-
+  
 include_recipe 'ldap389::service'
 
 service 'dirsrv' do

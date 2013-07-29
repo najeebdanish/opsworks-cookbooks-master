19b56d5192bf41fe2896e9ea01855378263ef9af
Palisado <budiha@hotmail.com>
2013-07-29 08:20:31 -0400

    LdapClient01

---
+++ ldap389/recipes/client.rb
@@ -0,0 +1,24 @@
+package 'pam_ldap'
+package 'nss-pam-ldapd'
+
+
+script "Ldap client setup" do
+  interpreter "bash"
+  user "root"
+  cwd "/etc/"
+  code <<-EOH
+  cat /etc/ssh/sshd_config | grep PasswordAuthentication | sed -i "s/PasswordAuthentication/#PasswordAuthentication/g" /etc/ssh/sshd_config
+  cat /etc/ssh/sshd_config | grep PasswordAuthentication | sed -i "s/UseDNS/#UseDNS/g" /etc/ssh/sshd_config
+  echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+  echo "UseDNS no" >> /etc/ssh/sshd_config
+  /etc/init.d/sshd restart
+  /etc/init.d/nslcd restart
+  authconfig --enableldap --enableldapauth --ldapserver='ldap://10.96.189.128/' --ldapbasedn='dc=evolv,dc=com' --enablelocauthorize --update
+  echo "%evolvadmins ALL=(ALL) /bin/su - evolv" >> /etc/sudoers
+  echo "%sysadmins        ALL=(ALL)       ALL" >> /etc/sudoers
+  echo "%Install       ALL=(ALL)       ALL" >> /etc/sudoers
+  echo "nameserver 10.96.189.128" >> /etc/resolv.conf
+  EOH
+end
+
+

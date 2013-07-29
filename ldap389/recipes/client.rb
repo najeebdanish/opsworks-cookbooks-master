e13d1b967fd5436a13641adfdc85c277e27da728
Palisado <budiha@hotmail.com>
2013-07-29 18:53:40 -0400

    setupBind01

--- ldap389/recipes/client.rb
+++ ldap389/recipes/client.rb
@@ -1,26 +1,42 @@
 package 'pam_ldap'
 package 'nss-pam-ldapd'
 package 'amanda-client'
+package 'expect'
+
+cookbook_file "/root/opsworks_amandarestore" do
+  source "opsworks_amandarestore"
+end
+
+cookbook_file "/etc/amanda/amanda-client.conf" do
+  source "amanda-client.conf"
+end
 
 script "Ldap client setup" do
   interpreter "bash"
   user "root"
   cwd "/etc/"
   code <<-EOH
+  sleep 30
+  chmod 600 /root/opsworks_amandarestore
+  chown root.root /root/opsworks_amandarestore
   cat /etc/ssh/sshd_config | grep PasswordAuthentication | sed -i "s/PasswordAuthentication/#PasswordAuthentication/g" /etc/ssh/sshd_config
   cat /etc/ssh/sshd_config | grep PasswordAuthentication | sed -i "s/UseDNS/#UseDNS/g" /etc/ssh/sshd_config
   echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
   echo "UseDNS no" >> /etc/ssh/sshd_config
   /etc/init.d/sshd restart
   /etc/init.d/nslcd restart
-  authconfig --enableldap --enableldapauth --ldapserver='ldap://10.96.189.128/' --ldapbasedn='dc=evolv,dc=com' --enablelocauthorize --update
+  authconfig --enableldap --enableldapauth --ldapserver='ldap://ldapserver1/' --ldapbasedn='dc=evolv,dc=com' --enablelocauthorize --update
   echo "%evolvadmins ALL=(ALL) /bin/su - evolv" >> /etc/sudoers
   echo "%sysadmins        ALL=(ALL)       ALL" >> /etc/sudoers
   echo "%Install       ALL=(ALL)       ALL" >> /etc/sudoers
-  echo "nameserver 10.96.189.128" >> /etc/resolv.conf
+  dnsip=`ping -c 1 ldapserver1 | grep icmp | awk -F"(" '{print$2}' | awk -F")" '{print $1}'`
+  echo "nameserver $dnsip" >> /etc/resolv.conf
   groupadd -g 300 evolv
   useradd -g 300 -u 300 -s /bin/bash -m -d /opt/evolv evolv
+  echo "iadbackup01.evolvsuite.local amandabackup amdump" >> /var/lib/amanda/.amandahosts
+  chown amandabackup.disk /etc/amanda/amanda-client.conf
   EOH
 end
+
 
 
--- ldap389/recipes/server.rb
+++ ldap389/recipes/server.rb
@@ -1,3 +1,23 @@
+cookbook_file "/root/amanda-backup_server-3.3.1-1.rhel6.x86_64.rpm" do
+  source "amanda-backup_server-3.3.1-1.rhel6.x86_64.rpm"
+end
+
+execute 'install amanda-backup-server' do
+  cwd '/root/'
+  command "rpm -i amanda-backup_server-3.3.1-1.rhel6.x86_64.rpm"
+  action :run
+  #only_if "rpm -qa|grep s3cmd"
+end
+
+cookbook_file "/root/opsworks_amandarestore.pub" do
+  source "opsworks_amandarestore.pub"
+end
+
+cookbook_file "/etc/amanda_IadBackup01.tar.gz" do
+  source "amanda_IadBackup01.tar.gz"
+end
+
+
 package '389-ds'
 
 remote_file "/root/s3cmd-1.0.0-4.1.x86_64.rpm" do
@@ -27,21 +47,14 @@ package 's3cmd'
 package 'xinetd'
 package 'bind'
 
-cookbook_file "/root/amanda-backup_server-3.3.1-1.rhel6.x86_64.rpm" do
-  source "amanda-backup_server-3.3.1-1.rhel6.x86_64.rpm"
+cookbook_file "/etc/named.conf" do
+  source "named.conf"
 end
 
-
-execute 'install amanda-backup-server' do
-  cwd '/root/'
-  command "rpm -i amanda-backup_server-3.3.1-1.rhel6.x86_64.rpm"
-  action :run
-  #only_if "rpm -qa|grep s3cmd"
+cookbook_file "/etc/bind_dcans01_29JUL13.tar.gz" do
+  source "bind_dcans01_29JUL13.tar.gz"
 end
 
-cookbook_file "/etc/amanda_IadBackup01.tar.gz" do
-  source "amanda_IadBackup01.tar.gz"
-end
 
 
 script "Get all 389 files" do
@@ -49,6 +62,10 @@ script "Get all 389 files" do
   user "root"
   cwd "/etc/"
   code <<-EOH
+  chown root.named /etc/named.conf
+  chmod 640 /etc/named.conf
+  tar -zxf bind_dcans01_29JUL13.tar.gz
+  /etc/init.d/named restart
   s3cmd get s3://opsworks-test01/dirsrv_etc.tar.gz
 #  tar -zxf dirsrv_etc.tar.gz
   cd /usr/lib64
@@ -76,10 +93,20 @@ script "Get all 389 files" do
   rm -rf amanda/
   tar -zxf amanda_IadBackup01.tar.gz
   /etc/init.d/xinetd restart
+  cat /root/opsworks_amandarestore.pub >> /root/.ssh/authorized_keys
+  iadbkp=`cat /etc/bind/zones/evolvsuite.local|grep iadbackup01`
+  ipadd=`ifconfig eth0 | grep "inet addr" | awk -F":" '{print $2}' | awk -F" " '{print $1}'`
+  iadbkpip=`echo $iadbkp | awk -F" " '{print $4}'`
+  newiadbkp=`echo $iadbkp | sed "s/$iadbkpip/$ipadd/"`
+  sed -i "s/$iadbkp/$newiadbkp/" /etc/bind/zones/evolvsuite.local
+  bindserial=`cat /etc/bind/zones/evolvsuite.local | grep Serial | awk -F" " '{print$1}'`
+  bindserialnew=`expr $bindserial + 1`
+  sed -i "s/$bindserial/$bindserialnew/" /etc/bind/zones/evolvsuite.local
+  rndc reload
   EOH
 end
 
-  
+
 include_recipe 'ldap389::service'
 
 service 'dirsrv' do

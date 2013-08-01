fd75fc7387ca56be3aedaabce8898e12dc2c7abe
najeebdanish <najeebdanish@gmail.com>
2013-08-01 14:46:34 -0400

    Update deploy.rb

--- Jboss/recipes/deploy.rb
+++ Jboss/recipes/deploy.rb
@@ -2,4 +2,11 @@ cookbook_file "/root/deploy.sh" do
   source "deploy.sh"
 end
 
-system('cd')
+script "Run the test script" do
+  interpreter "bash"
+  user "root"
+  cwd "/root"
+  code <<-EOH
+sh deploy.sh
+EOH
+end
